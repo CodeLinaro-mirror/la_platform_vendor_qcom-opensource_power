e8ec4b1c064fcf65232f359b8709cc3a3d0cfb65
Semi <quic_semipark@quicinc.com>
2024-08-14 16:59:38 -0700

    ADPF: update perfLockBoost
    called by reportActualWorkDuration.
    Apply min freq to help pass ADPF CTS testcase
    
    Change-Id: I809dfd11bcbed2ea198ae951192a89afa64609bc

--- PowerHintSession.cpp
+++ PowerHintSession.cpp
@@ -15,8 +15,8 @@
 #define MAX_BOOST 200
 #define MIN_BOOST -200
 #define PERFLOCK_MAX_THREADS 5
-#define PERFLOCK_MAX_BOOST_PCT 50
-#define PERFLOCK_MIN_BOOST_PCT -20
+#define PERFLOCK_MAX_BOOST_PCT 80
+#define PERFLOCK_MIN_BOOST_PCT -80
 #define PERFLOCK_BOOST_INC_PCT 10
 #define PERFLOCK_BOOST_DEC_PCT -10
 
@@ -90,10 +90,31 @@ bool PowerHintSessionImpl::perfLockBoost(int boostVal, int hintType) {
     }
 
     tBoostSumPerfLock += boostVal;
-    if(tBoostSumPerfLock > PERFLOCK_MAX_BOOST_PCT)
+    if(tBoostSumPerfLock > PERFLOCK_MAX_BOOST_PCT) {
         tBoostSumPerfLock = PERFLOCK_MAX_BOOST_PCT;
-    else if(tBoostSumPerfLock < PERFLOCK_MIN_BOOST_PCT)
+        if(mHandleFreq < 0) {
+            // Change the min freq to 800MHz
+            int listFreq[2];
+            listFreq[0] = 0x40800000;
+            listFreq[1] = 800;
+            mHandleFreq = interaction_with_handle(mHandleFreq, 3000, 2, listFreq);
+            if(mHandleFreq < 0) {
+                LOG(ERROR) << "Unable to acquire Perf lock.";
+            }
+        }
+    }
+    else {
+        if(mHandleFreq > 0) {
+            release_request(mHandleFreq);
+            mHandleFreq = -1;
+        }
+    }
+    if(tBoostSumPerfLock < PERFLOCK_MIN_BOOST_PCT)
         tBoostSumPerfLock = PERFLOCK_MIN_BOOST_PCT;
+
+    // release old boost
+    if(mHandlePerfLock > 0)
+        release_request(mHandlePerfLock);
 
     if(tBoostSumPerfLock != 0) {
         int numThreads = (mThreadIds.size() > PERFLOCK_MAX_THREADS) ? PERFLOCK_MAX_THREADS : mThreadIds.size();
@@ -111,9 +132,7 @@ bool PowerHintSessionImpl::perfLockBoost(int boostVal, int hintType) {
             return false;
         }
     }
-    // release old boost
-    if(mHandlePerfLock > 0)
-        release_request(mHandlePerfLock);
+
     mHandlePerfLock = handle;
     if(hintType != LOAD_RESET)
         mBoostSumPerfLock = tBoostSumPerfLock;
@@ -171,6 +190,7 @@ PowerHintSessionImpl::PowerHintSessionImpl(int32_t tgid, int32_t uid, const std:
      mBoostSumPerfLock = 0;
      mHandlePerfLock = -1;
      mLastActionPerfLock = -1;
+     mHandleFreq = -1;
 }
 
 PowerHintSessionImpl::~PowerHintSessionImpl(){
@@ -222,12 +242,12 @@ ndk::ScopedAStatus PowerHintSessionImpl::updateTargetWorkDuration(int64_t in_tar
     return ndk::ScopedAStatus::ok();
 }
 ndk::ScopedAStatus PowerHintSessionImpl::reportActualWorkDuration(const std::vector<::aidl::android::hardware::power::WorkDuration>& in_durations){
-    LOG(INFO) << "reportActualWorkDuration ";
     int64_t prevActualDurations = mPrevActualWorkDurationNanos;
     int64_t actualDurations = in_durations.back().durationNanos;
     int64_t targetDurations = mTargetWorkDurationNanos;
     int boostSum = mBoostSumPerfLock;
     int lastAction = mLastActionPerfLock;
+    LOG(INFO) << "reportActualWorkDuration " << actualDurations;
 
     if(actualDurations >= targetDurations) {
         if((lastAction == LOAD_UP || lastAction == LOAD_RESUME) && actualDurations < prevActualDurations)
@@ -250,6 +270,10 @@ ndk::ScopedAStatus PowerHintSessionImpl::pause(){
         setSessionActivity(this, false);
         sendHint(aidl::android::hardware::power::SessionHint::CPU_LOAD_RESET);
         sendHintPerfLock(aidl::android::hardware::power::SessionHint::CPU_LOAD_RESET);
+        if(mHandleFreq > 0) {
+            release_request(mHandleFreq);
+            mHandleFreq = -1;
+        }
         removePipelining();
     }
     return ndk::ScopedAStatus::ok();
@@ -270,6 +294,10 @@ ndk::ScopedAStatus PowerHintSessionImpl::close(){
     if(isSessionAlive(this)) {
         sendHint(aidl::android::hardware::power::SessionHint::CPU_LOAD_RESET);
         sendHintPerfLock(aidl::android::hardware::power::SessionHint::CPU_LOAD_RESET);
+        if(mHandleFreq > 0) {
+            release_request(mHandleFreq);
+            mHandleFreq = -1;
+        }
         removePipelining();
         mThreadIds.clear();
 
