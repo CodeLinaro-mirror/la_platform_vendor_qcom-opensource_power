395c3ddd07cf0e1b3989c2313d166863ef4499ba
Aditya Singh <adsing@qti.qualcomm.com>
2025-11-14 12:08:35 +0530

    Power: camera powerhint initial tuning
    Camera powerhint initial tuning for chora (Skyros)
    
    Change-Id: I123c65e738a95b977ba6b3834f7a629f613bfe11

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -35,4 +35,6 @@ else ifeq ($(TARGET_BOARD_PLATFORM),niobe)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/niobe/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),parrot)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/parrot/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),chora)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/chora/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
