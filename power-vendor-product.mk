d711f3ce2527c96b2ad6539e6bab65a67cdd2d1a
Pawan Prasad <pawanpra@qti.qualcomm.com>
2026-05-22 14:57:22 +0530

    power:- add powerhint file for shikra CQ2390
    Added powerhint.xml for CQ2390 for shikra
    
    Change-Id: If96803d699e23062e6260aa35d78767a84ba88ee

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -47,4 +47,6 @@ else ifeq ($(TARGET_BOARD_PLATFORM),seraph)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/seraph/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),pikachu)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/pikachu/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),shikra)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/shikra/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
